fda23a7c24d3f72b402a42ae0edcc32e310334fe
Cēsu Kultūras centrs <80533818+cesis@users.noreply.github.com>
2021-03-28 15:18:10 +0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -902,7 +902,7 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
+    var credit = 'Autors: ';
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
 
@@ -910,14 +910,14 @@ $(window).on('load', function() {
       if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
       credit += ' by <a href="' + url + '">' + name + '</a> | ';
     } else if (name) {
-      credit += ' by ' + name + ' | ';
+      credit +=  name + ' | ';
     } else {
       credit += ' | ';
     }
 
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
+    credit += 'Kods';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit += ' with ';
+    credit += ' no ';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
 
